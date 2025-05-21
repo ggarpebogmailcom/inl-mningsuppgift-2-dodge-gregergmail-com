e7cfd0cb4766657df89ed43e7f9507de0df7bc39
dodge-gregergmail-com <dodge.greger@gmail.com>
2025-05-21 19:38:34 +0200

    Klar

--- src/script.js
+++ src/script.js
@@ -1,26 +1,58 @@
 // Definera array av namn
-let people;
+let people = [
+    "Owen", 
+    "Mason", 
+    "Liam", 
+    "Ella", 
+    "Adam", 
+    "Noah",
+    "Ava", 
+    "Ethan", 
+    "Grace", 
+    "Lucas", 
+    "Zoe", 
+    "Jack",
+    "Lily", 
+    "Henry", 
+    "Emma", 
+    "Isaac", 
+    "Mia", 
+    "Caleb",
+    "Ruby", 
+    "Leo"
+];
 
 
 export function initSearch(people) { // rör ej denna rad
 
   // Hämta HTML-element
+  const searchBar = document.getElementById("searchInput")
+  const searchButton = document.getElementById("searchBtn")
+  const resultHolder = document.getElementById("resultDisplay")
   
   // Lägg till en klickhändelse på "Sök"-knappen
   searchBtn.addEventListener("click",() => {
   
-    // 1. Hämta texten i searchInput.value
+    let inputText = document.getElementById("searchInput").value;
+
+    let found = false;
+
+    for (let person of people) {
+      
+      if (inputText === person) {
+        found = true;
+      }
+    } 
+
+    if (found) {
+
+       resultHolder.innerHTML = "Namn hittades!";
+      } else { 
+        resultHolder.innerHTML = "Namn hittades inte.";
+        found = false;
+      }
     
-    // 2. Skapa en boolean för found
-    
-    // 3. Loopa igenom people-arrayen
-    
-    // 4. Jämför texten med varje namn i arrayen
-    
-    // 5. Om du hittar en match, skriv ut "Namn hittades!" i resultDisplay, ändra found till true
-    
-    // 6. Om du efter loopen inte hittat något, skriv ut "Namn hittades inte." och gör found till false igen.
-    
+
     });
 }
 
